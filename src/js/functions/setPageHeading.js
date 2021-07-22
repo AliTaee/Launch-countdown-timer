0233e1178238d84435adefc96de4dafb34cebf65
alitaee <ali.taee.72@gmail.com>
2021-07-22 13:38:11 +0430

    set main title if value is undefined

--- src/js/functions/setPageHeading.js
+++ src/js/functions/setPageHeading.js
@@ -1,4 +1,4 @@
-export const setPageHeading = (title = '') => {
+export const setPageHeading = (title = "We're launching soon") => {
   const pageHeadingElement = document.getElementById('main-title')
 
   if (pageHeadingElement) {
